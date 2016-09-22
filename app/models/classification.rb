a13b32747d952778276d424c4f15fda77387e267
fidelscodes <fidelscodes@gmail.com>
2016-09-22 17:57:01 -0400

    pass classification tests

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    max_length = Boat.maximum(:length)
+    classification_ids = Boat.where(length: max_length).first.classifications.map(&:id)
+    where(id: classification_ids)
+  end
 end
